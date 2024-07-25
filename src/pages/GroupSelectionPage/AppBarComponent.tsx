cd533a8b54354e88a6fd4058f94bb169e28bddb3
jidnjs <jkang.2005@icloud.com>
2024-07-25 17:44:31 +0900

    Logo added

--- src/pages/GroupSelectionPage/AppBarComponent.tsx
+++ src/pages/GroupSelectionPage/AppBarComponent.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Button, Divider } from '@mui/material';
+import logo from '../../../public/Flower_logo.png';
 
 interface AppBarComponentProps {
   userName: string;
   handleLogout: () => void;
 }
-
 const AppBarComponent: React.FC<AppBarComponentProps> = ({ userName, handleLogout }) => {
   return (
     <>
       <AppBar position="static" style={{ backgroundColor: '#fff', height: 80, justifyContent: 'center' }}>
         <Toolbar>
+        <img src={logo} alt="Logo" style={{ marginRight: 16, height: 55 }} />
           <Typography variant="h6" style={{ flexGrow: 1, color: '#000' }}>
             Welcome, {userName}
           </Typography>
